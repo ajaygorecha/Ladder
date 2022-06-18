cf69956320a24ef7ab09e00eab48b574fe7bf9c9
jayminkarthiya <karathiyajayaminp@gmail.com>
2022-06-18 14:43:14 +0530

    bar chart css

--- src/app/bar-chart/bar-chart.component.ts
+++ src/app/bar-chart/bar-chart.component.ts
@@ -31,7 +31,28 @@ export class BarChartComponent implements OnInit {
         datasets: this.lineChartData.datasets,
       },
       options: {
+        plugins: {
+          legend: {
+            position: 'bottom',
+            labels: { usePointStyle: true, pointStyle: 'circle' },
+          },
+          title: {
+            align: 'start',
+            display: true,
+            font: { weight: 'bolder', size: 20 },
+            text: 'Chart Title',
+            padding: {
+              top: 20,
+              bottom: 40,
+            },
+          },
+        },
         scales: {
+          x: {
+            grid: {
+              display: false,
+            },
+          },
           y: {
             beginAtZero: true,
           },
