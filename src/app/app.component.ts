fe1408e220016ffd56c8d497ecb3de8af25d4015
jayminkarthiya <karathiyajayaminp@gmail.com>
2022-06-24 23:50:03 +0530

    default chart selection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     setTimeout(() => {
-      this.hideShowBarOrLineChart('bar');
+      this.hideShowBarOrLineChart('score');
     }, 400);
   }
 
